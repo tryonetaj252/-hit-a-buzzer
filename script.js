a61fe1ab8ed23f2a1713312d1fc3871cde3e8026
Tyronetaj252 <tyronetajwilliams@aol.com>
2024-04-12 14:51:35 -0400

    updated

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 let clickButtonCount = 0;
 let totalCookies = 0;
 
-function updateMessage (){
+function updateMessage (cookieType) {
     const messageDiv = document.getElementById('message');
     clickButtonCount++;
     totalCookies++;
     if (totalCookies === 20){
     messageDiv.textContent = "Congratulations! You Win the game!"; 
  } else {
-    messageDiv.textContent = "Congratulations! You won a cookie. 🍪 Total Cookies earned:"+ totalCookies;
+    messageDiv.textContent = "Congratulations! You won a " + cookieType + "cookie. 🍪 Total Cookies earned:"+ totalCookies;
  }
 
 }
-document.getElementById("clickButton").addEventListener('click', updateMessage)
+
     
+document.getElementById("cookieButton1").addEventListener('click', function() {
+    updateMessage("Chocolate");
+});
+
+document.getElementById("cookieButton2").addEventListener('click',function() {
+    updateMessage("Vanilla");
+});
+
+document.getElementById("cookieButton3").addEventListener('click', function() {
+    updateMessage("Strawberry");
+});
+
+
+
+
+
+
+
